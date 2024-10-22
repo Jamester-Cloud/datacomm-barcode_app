41493c128047b9b2db3b572604f9cbeed00368a1
edison <edisonrosalescampos@gmail.com>
2024-10-22 12:08:13 -0400

    cambios pendientes

---
+++ list.js
@@ -0,0 +1,27 @@
+const barcodeList =[
+    {
+      "code": "1234567890128",
+      "formatType": "ean13",
+      "quantity": 10
+    },
+    {
+      "code": "12345670",
+      "formatType": "ean8",
+      "quantity": 10
+    },
+    {
+      "code": "12345",
+      "formatType": "ean5",
+      "quantity": 10
+    },
+    {
+      "code": "12",
+      "formatType": "ean2",
+      "quantity": 10
+    },
+    {
+      "code": "123456789012",
+      "formatType": "upc",
+      "quantity": 10
+    }
+];
